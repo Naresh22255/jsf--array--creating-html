2b8d78e08c5912f27302c58705f2b8544af7a846
naresh22255 <naresh22255@gmail.com>
2020-03-03 13:23:00 +0100

    Done

--- js/2-multitags.js
+++ js/2-multitags.js
@@ -1,5 +1,3 @@
-
-
 /************
  * EXAMPLE
  * Creating HTML from data - Create a function called renderArticle that takes in an object and returns the data as HTML
@@ -7,15 +5,16 @@
 
 // starting data
 const article = {
-    id: 123,
-    title: "First kitten catches Coronavirus",
-    intro: "Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nulla porta dolor.",
-    imageUrl: "http://placekitten.com/300/150"
+  id: 123,
+  title: "First kitten catches Coronavirus",
+  intro:
+    "Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Nulla porta dolor.",
+  imageUrl: "http://placekitten.com/300/150"
 };
 
 // function to convert from data to HTML string
 function renderArticle(articleData) {
-    return `
+  return `
         <article data-articleid="${articleData.id}">
             <h1>${articleData.title}</h1>
             <img src="${articleData.imageUrl}" alt="${articleData.title} image" />
@@ -29,8 +28,6 @@ var htmlString = renderArticle(article);
 // add the HTML into the page
 document.body.innerHTML += htmlString;
 
-
-
 /************
  * Creating HTML from data - Create a function called renderCatstagramPost that takes in an object and returns the data as HTML.
  * The start of the functions has been done for you.
@@ -39,26 +36,34 @@ document.body.innerHTML += htmlString;
 
 // starting data
 const catstagramPost = {
-    id: 234,
-    caption: "Aenean tellus metus, bibendum sed. #posuere #ac #mattis #non",
-    image: "http://placekitten.com/300/300",
-    numberOfLikes: 256,
+  id: 234,
+  caption: "Aenean tellus metus, bibendum sed. #posuere #ac #mattis #non",
+  image: "http://placekitten.com/300/300",
+  numberOfLikes: 256
 };
 
 // function to convert from data to HTML string
 function renderCatstagramPost(postData) {
-    return `
+  return `
         <div data-postid="${postData.id}">
+        <h2>${postData.caption}</h2>
            <!-- ***** ADD EXTRA HTML HERE **** -->
         </div>
+        <img src="${postData.image}"/>
+        <p>'Number of Likes'${postData.numberOfLikes}</p>
     `;
 }
 
+// create HTML from the data
+var htmlString = renderCatstagramPost(catstagramPost);
+
+// add the HTML into the page
+document.body.innerHTML += htmlString;
 
 /************
  * Creating HTML from data - Create a function called renderAdvert that takes in an object and returns the data as HTML.
  * THEN add the post to the page using the method from the previous questions.
- * 
+ *
  * Use the HTML format:
  * <article data-advertid="ID">
  *   <h2>TITLE</h2>
@@ -68,10 +73,27 @@ function renderCatstagramPost(postData) {
  ************/
 
 // starting data
+// function to convert from data to HTML string
 const advert = {
-    id: 234,
-    title: "Used car for sale",
-    price: 30000,
-    description: "Nullam sagittis. Praesent venenatis metus at tortor pulvinar varius. Morbi mattis ullamcorper velit. Praesent turpis."
+  id: 234,
+  title: "Used car for sale",
+  price: 30000,
+  description:
+    "Nullam sagittis. Praesent venenatis metus at tortor pulvinar varius. Morbi mattis ullamcorper velit. Praesent turpis."
 };
 
+function renderAdvert(data) {
+  return `
+    <article data-advertid=${data.id}>
+     <h2>${data.title}</h2>
+     <div>${data.price}</div>
+     <p>${data.description}</p>
+   </article>
+    `;
+}
+
+// create HTML from the data
+var htmlString = renderAdvert(advert);
+
+// add the HTML into the page
+document.body.innerHTML += htmlString;
